1ab79b819ecf620e978f9d2b54250d206a97fee8
Seyoung <seyoung2k@naver.com>
2019-08-10 23:03:37 -0700

    new

--- Aus10/gitTest.java
+++ Aus10/gitTest.java
@@ -2,6 +2,17 @@ package Aus10;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.print("I am so sexy");
+        int[][] data = {{1,2,3},{4,5,6}};
+
+        for(int i = 0; i < data[0].length; i++) {
+            int blank = data[1][i];
+            data[1][i] = data[0][i];
+            data[0][i] = blank;
+        }
+        for(int i = 0; i < data.length; i++) {
+            for(int j = 0; j < data[0].length; j++) {
+                System.out.print(data[i][j]);
+            }
+        }
     }
 }
