42387b05146f2e6c5e5d6b1ad09975598a0e9005
Seyoung <seyoung2k@naver.com>
2019-08-14 14:24:18 +0900

    HW

---
+++ Aus10/HW01.java
@@ -0,0 +1,14 @@
+package Aus10;
+
+public class HW01 {
+    public static void main(String[] args) {
+        int[][]arr = {{1,2,3},{4,5,6},{7,8,9}};
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[arr.length - 1 - i][arr[0].length - 1 - j]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Aus10/HW02.java
@@ -0,0 +1,14 @@
+package Aus10;
+
+public class HW02 {
+    public static void main(String[] args) {
+        int[][]arr = {{1,2,3},{4,5,6},{7,8,9}};
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[arr.length - 1 - j][i]);
+            }
+            System.out.println();
+        }
+    }
+}
