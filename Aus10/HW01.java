58c0c98b297a91e00669ef8a3efe29654b0a239b
Seyoung <seyoung2k@naver.com>
2019-08-19 11:10:31 +0900

    new

--- Aus10/HW01.java
+++ Aus10/HW01.java
@@ -3,10 +3,16 @@ package Aus10;
 public class HW01 {
     public static void main(String[] args) {
         int[][]arr = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][]newArr = new int [3][3];
 
         for(int i = 0; i < arr.length; i++) {
             for(int j = 0; j < arr[0].length; j++) {
-                System.out.print(arr[arr.length - 1 - i][arr[0].length - 1 - j]);
+               newArr[arr.length - 1 - i][arr[0].length - 1 - j] = arr[i][j];
+            }
+        }
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[0].length; j++) {
+                System.out.print(newArr[i][j]);
             }
             System.out.println();
         }
---
+++ Aus10/HW01MORE.java
@@ -0,0 +1,16 @@
+package Aus10;
+
+import java.util.Scanner;
+
+public class HW01MORE {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of First dimension: ");
+        int firstDimension = sc.nextInt();
+        System.out.print("Enter size of Second dimension: ");
+        int secondDimension = sc.nextInt();
+        int[][]arr = new int [firstDimension][secondDimension];
+
+
+    }
+}
