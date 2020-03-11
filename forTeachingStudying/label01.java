ea7e2374b53bd2245f92da5e4ff89b53dd425cf3
Seyoung <seyoung2k@naver.com>
2020-03-11 14:44:55 -0700

    For studying

---
+++ forTeachingStudying/label01.java
@@ -0,0 +1,24 @@
+package forTeachingStudying;
+
+public class label01 {
+    public static void main(String[] args) {
+        label:
+        for (int i = 1; i < 6; ++i) {
+            for (int j = 1; j < 5; ++j) {
+                if (i == 3 || j == 2)
+                    continue label;
+                System.out.println("i = " + i + "; j = " + j);
+            }
+        }
+    }
+}
+/*
+1. homework first and test.
+
+2. continue and break + label
+
+print odd number between 1~10 with using continue
+
+using for and while
+
+ */
