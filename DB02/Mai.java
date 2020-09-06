4b8028de40ee77c1a9f97677857b21284a939bae
Seyoung <seyoung2k@naver.com>
2020-09-06 21:10:38 +0900

    new file

---
+++ DB02/Mai.java
@@ -0,0 +1,16 @@
+package DB02;
+
+public class Mai {
+    public static void main(String[] args) {
+        ArrayList numbers = new ArrayList();
+        numbers.addLast(10);
+        numbers.addLast(20);
+        numbers.addLast(30);
+        numbers.addLast(40);
+        numbers.add(1, 15);
+        numbers.addFirst(5);
+
+        System.out.println(numbers);
+
+    }
+}
