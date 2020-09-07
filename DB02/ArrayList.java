ed5d8c26e28f1251e129fffd8f08820396708025
Seyoung <seyoung2k@naver.com>
2020-09-07 20:45:40 +0900

    new file

--- DB02/ArrayList.java
+++ DB02/ArrayList.java
@@ -21,4 +21,43 @@ public class ArrayList {
 
         return add(0, element);
     }
+    public String toString() {
+        String str = "[";
+        for(int i = 0; i < size; i++) {
+            str += elementData[i];
+            if(i < size - 1) {
+                str += ",";
+            }
+        }
+        return str + "]";
+    }
+    public Object remove(int index) {
+        Object removed = elementData[index];
+        for(int i = index + 1; i < size - 1; i++) {
+            elementData[i - 1] = elementData[i];
+        }
+        size--;
+        elementData[size] = null;
+        return removed;
+    }
+    public Object removeFirst() {
+        return remove(0);
+    }
+    public Object removeLast() {
+        return remove(size - 1);
+    }
+    public Object get(int index) {
+        return elementData[index];
+    }
+    public int size() {
+        return size;
+    }
+    public Object indexOf(Object o) {
+        for(int i = 0; i < size; i++) {
+            if(o.equals(elementData[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
--- DB02/Mai.java
+++ DB02/Mai.java
@@ -7,10 +7,14 @@ public class Mai {
         numbers.addLast(20);
         numbers.addLast(30);
         numbers.addLast(40);
-        numbers.add(1, 15);
-        numbers.addFirst(5);
-
+        //numbers.add(1, 15);
+        //numbers.addFirst(5);
+        numbers.removeFirst();
+        numbers.removeLast();
+        System.out.println(numbers.get(0));
+        System.out.println(numbers.size());
+        System.out.println(numbers.indexOf(20));
         System.out.println(numbers);
-
+        System.out.println(numbers.remove(1));
     }
 }
